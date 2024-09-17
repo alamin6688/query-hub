4cfe4261fba7542023989f31382a67b513a755d5
alamin6688 <alamin.kzs06@gmail.com>
2024-09-18 00:25:32 +0600

    Banner section modified

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -10,8 +10,9 @@ import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import Slide from "./Slide";
 
-import bgImg01 from "../../../assets/blogPostImages/Manage Product Queries.jpg";
-import bgImg02 from "../../../assets/blogPostImages/Comparing Products.jpg";
+import bgImg01 from "../../../assets/blogPostImages/Product Recommendations in 2024.jpg";
+import bgImg02 from "../../../assets/blogPostImages/Manage Product Queries.jpg";
+import bgImg03 from "../../../assets/blogPostImages/Comparing Products.jpg";
 
 export default function Carousel() {
   return (
@@ -21,7 +22,7 @@ export default function Carousel() {
           spaceBetween={30}
           centeredSlides={true}
           autoplay={{
-            delay: 3000,
+            delay: 3500,
             disableOnInteraction: false,
           }}
           pagination={{
@@ -34,25 +35,25 @@ export default function Carousel() {
           <SwiperSlide>
             <Slide
               image={bgImg01}
+              text="Join Our Community and Share Your Product Insights"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Slide
+              image={bgImg02}
               text="Manage Your Product Queries and Recommendations"
             />
           </SwiperSlide>
           <SwiperSlide>
             <Slide
-              image={bgImg02}
+              image={bgImg03}
               text="Compare Products and Find the Best Alternatives"
             />
           </SwiperSlide>
           <SwiperSlide>
             <Slide
-              image={bgImg01}
-              text="Discover Alternatives for Your Favorite Products"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Slide
-              image={bgImg02}
-              text="Join the Community and Share Your Product Insights"
+              image="https://i.postimg.cc/X7YzJVGh/adult-male-illustrator-working-tablet-device.jpg"
+              text="Connect, Collaborate, and Elevate Your Product Knowledge!"
             />
           </SwiperSlide>
         </Swiper>
--- src/Pages/Home/Banner/Slide.jsx
+++ src/Pages/Home/Banner/Slide.jsx
@@ -10,12 +10,12 @@ const Slide = ({ image, text }) => {
     >
       <div className="flex items-center justify-center w-full h-full bg-gray-900/70">
         <div className="flex items-center flex-col justify-center">
-          <h1 className="text-xl md:text-3xl w-[80%] mx-auto font-semibold text-white lg:text-4xl text-center">
+          <h1 className="text-xl md:text-3xl w-[80%] mx-auto font-semibold text-white lg:text-4xl text-center font-poppins">
             {text}
           </h1>
           <br />
           <Link to="/queries">
-            <button className="relative inline-flex h-12 transition-transform duration-300 transform overflow-hidden rounded-lg p-[1px] focus:outline-none bg-slate-950 text-white hover:scale-105 hover:bg-slate-800">
+            <button className="relative font-poppins inline-flex h-12 transition-transform duration-300 transform overflow-hidden rounded-lg p-[1px] focus:outline-none bg-slate-950 text-white hover:scale-105 hover:bg-slate-800">
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#0000ff_0%,#00ffff_50%,#0000ff_100%)]"></span>
               <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium backdrop-blur-3xl gap-2">
                 Explore Queries
