d5a29dafa6790e78eaa107d0286b00ff015bf85a
alamin6688 <alamin.kzs06@gmail.com>
2024-09-28 21:52:47 +0600

    My Queries Delete functionality added

--- src/Pages/MyQueries/MyQueries.jsx
+++ src/Pages/MyQueries/MyQueries.jsx
@@ -3,13 +3,20 @@ import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
-import { FaPlusCircle } from "react-icons/fa";
+import { FaPlusCircle, FaEye, FaEdit } from "react-icons/fa";
+import toast from "react-hot-toast";
+import Swal from "sweetalert2";
+import { BiEdit } from "react-icons/bi";
 
 const MyQueries = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const { data: queries = [], isLoading } = useQuery({
+  const {
+    data: queries = [],
+    isLoading,
+    refetch,
+  } = useQuery({
     queryFn: () => getData(),
     queryKey: ["queries", user?.email],
   });
@@ -26,6 +33,40 @@ const MyQueries = () => {
   const filteredQueries = queries.filter(
     (query) => query.userEmail === user?.email
   );
+
+  // Handle Delete
+  const handleDelete = async (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+      customClass: {
+        popup: "bg-[#FFFFFF] rounded-xl",
+      },
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const { data } = await axiosSecure.delete(`/myQueries/${id}`);
+          console.log(data);
+
+          // Display Success Toast
+          toast.success("Delete Successful.");
+
+          // Refresh UI
+          refetch();
+        } catch (err) {
+          console.log(err.message);
+          toast.error(err.message);
+        }
+      } else {
+        toast.error("Delete canceled!");
+      }
+    });
+  };
 
   return (
     <>
@@ -87,19 +128,40 @@ const MyQueries = () => {
                       {query.currentDate}
                     </p>
 
-                    <div className="flex items-center justify-between mt-4">
+                    <div className="flex items-center gap-2 justify-between mt-4">
+                      {/* View Details Button */}
                       <Link to={`/queries/${query._id}`}>
-                        <button className="btn btn-primary">
-                          View Details
+                        <button className="inline-flex items-center px-4 py-2 bg-green-600 transition ease-in-out delay-75 hover:bg-green-700 text-white text-sm font-medium rounded-md hover:-translate-y-1 hover:scale-110">
+                          <FaEye className="mr-2 text-xl" /> View Details
                         </button>
                       </Link>
+
+                      {/* Update Button */}
                       <Link to={`/update/${query._id}`}>
-                        <button className="btn btn-secondary">Update</button>
+                        <button className="inline-flex items-center px-4 py-2 bg-blue-600 transition ease-in-out delay-75 hover:bg-blue-700 text-white text-sm font-medium rounded-md hover:-translate-y-1 hover:scale-110">
+                          <FaEdit className="mr-2 text-xl" /> Update
+                        </button>
                       </Link>
+
+                      {/* Delete Button */}
                       <button
-                        className="btn btn-error"
                         onClick={() => handleDelete(query._id)}
+                        class="inline-flex items-center px-4 py-2 bg-red-600 transition ease-in-out delay-75 hover:bg-red-700 text-white text-sm font-medium rounded-md hover:-translate-y-1 hover:scale-110"
                       >
+                        <svg
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          class="h-6 w-6 mr-2"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                            stroke-width="2"
+                            stroke-linejoin="round"
+                            stroke-linecap="round"
+                          ></path>
+                        </svg>
                         Delete
                       </button>
                     </div>
