584d740663c2b838a3b7a3f3824a152ed7372774
alamin6688 <alamin.kzs06@gmail.com>
2024-10-21 22:48:36 +0600

    Contact Us Page data Updated

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -15,58 +15,77 @@ const Contact = () => {
                 Get in touch
               </h2>
               <p className="w-full md:w-2/3 mx-auto mt-4 text-center text-gray-900 font-poppins text-[18px]">
-                Our friendly team would love to hear from you. We are always here to assist with any questions or concerns you may have.
+                Our friendly team would love to hear from you. We are always
+                here to assist with any questions or concerns you may have.
               </p>
             </div>
 
             <div className="gap-8 mt-10 items-center flex justify-center">
               <div className="grid grid-cols-1 gap-8 lg:grid-cols-1 animate__animated animate__slideInUp">
-
                 <div className="text-center">
                   <span className="inline-block p-3 text-blue-500 rounded-full bg-blue-100/80">
                     <MdEmail className="w-5 h-5" />
                   </span>
-                  <h2 className="mt-4 text-base font-bold text-gray-800">Email</h2>
-                  <p className="mt-2 text-sm text-gray-500">Our friendly team is here to help.</p>
-                  <p className="mt-2 text-sm text-blue-500">hello@gmail.com</p>
+                  <h2 className="mt-4 text-base font-bold text-gray-800">
+                    Email
+                  </h2>
+                  <p className="mt-2 text-sm text-gray-500">
+                    Our friendly team is here to help.
+                  </p>
+                  <p className="mt-2 text-sm text-blue-500">
+                    alamin.kzs06@gmail.com
+                  </p>
                 </div>
-
 
                 <div className="text-center">
                   <span className="inline-block p-3 text-blue-500 rounded-full bg-blue-100/80">
                     <MdLocationOn className="w-5 h-5" />
                   </span>
-                  <h2 className="mt-4 text-base font-bold text-gray-800">Office</h2>
-                  <p className="mt-2 text-sm text-gray-500">Come say hello at our office HQ.</p>
+                  <h2 className="mt-4 text-base font-bold text-gray-800">
+                    Office
+                  </h2>
+                  <p className="mt-2 text-sm text-gray-500">
+                    Come say hello at our office HQ.
+                  </p>
                   <p className="mt-2 text-sm text-blue-500">
                     123 Gulshan Avenue, Dhaka 1212, Bangladesh
                   </p>
                 </div>
 
-
                 <div className="text-center">
                   <span className="inline-block p-3 text-blue-500 rounded-full bg-blue-100/80">
                     <MdPhone className="w-5 h-5" />
                   </span>
-                  <h2 className="mt-4 text-base font-bold text-gray-800">Phone</h2>
-                  <p className="mt-2 text-sm text-gray-500">Mon-Fri from 9am to 5pm.</p>
-                  <p className="mt-2 text-sm text-blue-500">+880 1234 567890</p>
+                  <h2 className="mt-4 text-base font-bold text-gray-800">
+                    Phone
+                  </h2>
+                  <p className="mt-2 text-sm text-gray-500">
+                    Mon-Fri from 9am to 5pm.
+                  </p>
+                  <p className="mt-2 text-sm text-blue-500">+8801836429252</p>
                 </div>
 
                 <div className="text-center">
                   <span className="inline-block p-3 text-blue-500 rounded-full bg-blue-100/80">
                     <MdWeb className="w-5 h-5" />
                   </span>
-                  <h2 className="mt-4 text-base font-bold text-gray-800">Website</h2>
-                  <p className="mt-2 text-sm text-gray-500">Visit our website for more information.</p>
+                  <h2 className="mt-4 text-base font-bold text-gray-800">
+                    Website
+                  </h2>
+                  <p className="mt-2 text-sm text-gray-500">
+                    Visit our website for more information.
+                  </p>
                   <p className="mt-2 text-sm text-blue-500">
-                    <a href="https://www.example.com" target="_blank" rel="noopener noreferrer">
+                    <a
+                      href="https://www.example.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       www.website.com
                     </a>
                   </p>
                 </div>
               </div>
-
             </div>
           </div>
         </section>
